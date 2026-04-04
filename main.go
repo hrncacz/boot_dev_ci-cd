0970742893fd9338c53915356b1a0206c8524248
hrncacz <hrncacz@gmail.com>
2026-04-04 14:22:09 +0200

    Resolving gosec issues

--- main.go
+++ main.go
@@ -89,10 +89,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:        ":" + port,
+		Handler:     router,
+		ReadTimeout: 5000,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	// log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
